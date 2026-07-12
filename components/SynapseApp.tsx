e01938cd634d2e15dbb85fc9e644cc035633bdac
Joyanne Ma <joyym18@gmail.com>
2026-07-11 20:37:24 -0400

    fixes

--- components/SynapseApp.tsx
+++ components/SynapseApp.tsx
@@ -325,6 +325,8 @@ export default function SynapseApp() {
       time: "now",
       colorIdx: 0,
       organizing: true,
+      isTodo: classifiedNote?.is_todo === true,
+      todoText: classifiedNote?.title ?? transcript,
     };
     setOverlayOpen(false);
     setIsRecording(false);
